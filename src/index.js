63b7a21b02560583ecb243618a8c7db948476ee2
Volodymyr <mospan.volodymyr@gmail.com>
2023-01-19 21:10:44 +0200

    mobile hero

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// let objectOne = { name: 'Bloom' };
+// console.log(objectOne);
+// let objectTwo = objectOne;
+// objectOne = { name: 'Oly' };
+// console.log(objectOne);
+// console.log(objectTwo);
+
+console.log(0 || 0 || 0);
